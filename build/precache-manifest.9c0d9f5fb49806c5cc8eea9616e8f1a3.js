b7fb5ee36b3d910e76ecaf6f8b6735f2be3f4f9b
jmathins <jmathins@aislerocket.com>
2021-06-13 16:39:04 +0500

    deploy config

---
+++ build/precache-manifest.9c0d9f5fb49806c5cc8eea9616e8f1a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9800341729be9706d567d1be8b8b4257",
+    "url": "/artistsevents/index.html"
+  },
+  {
+    "revision": "2a4552d66c773b25df2f",
+    "url": "/artistsevents/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "7c67cab246bdc8fc626a",
+    "url": "/artistsevents/static/css/main.2f1e6ce2.chunk.css"
+  },
+  {
+    "revision": "2a4552d66c773b25df2f",
+    "url": "/artistsevents/static/js/2.4d770850.chunk.js"
+  },
+  {
+    "revision": "c3607495a6efb5df9f601a58e50a5999",
+    "url": "/artistsevents/static/js/2.4d770850.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c67cab246bdc8fc626a",
+    "url": "/artistsevents/static/js/main.9df77708.chunk.js"
+  },
+  {
+    "revision": "51875131c7542525a9ce",
+    "url": "/artistsevents/static/js/runtime-main.6ee2e9ff.js"
+  }
+]);
